e43ae9365a0f78b25bf2f4831e416f110b422fe7
Sms-Rk <smsrk741@gmail.com>
2022-12-14 19:35:47 +0330

    bug fixes

--- grafana.py
+++ grafana.py
@@ -12,7 +12,8 @@ link_url = Template("https://grafana.tip.co.ir/d/rYdddlPWk/per-gpu-metrics?orgId
 with open('servers.txt') as f:
     for index, line in enumerate(f):
         server_id=(line.strip().split(',')[0])
-        server_num=(line.strip().split(',')[1])
+        server_num=(line.strip().split(',')[1][-3:])
+        print(server_num)
         rulenum = 0
         for rulenum in range(3):
             if rulenum == 0:
@@ -20,8 +21,8 @@ with open('servers.txt') as f:
                 rule["alias"] = "GPU Utils"
                 rule["linkData"][0]["pattern"] = server_id
                 rule["shapeData"][0]["pattern"] = server_id
-                rule["pattern"] = f"GPUnode{server_num}"
-                rule["tooltiplabel"] = "GPU Utils"
+                rule["pattern"] = f"GPUGPU{server_num}"
+                rule["tooltipLabel"] = "GPU Utils"
                 link = f"GPU{server_num}"
                 rule["linkData"][0]["linkUrl"] = link_url.substitute(server=link)
                 dashboard["panels"][0]["rulesData"]["rulesData"].append(rule)
@@ -31,8 +32,8 @@ with open('servers.txt') as f:
                 rule["alias"] = "Memory Usage"
                 rule["linkData"][0]["pattern"] = server_id
                 rule["shapeData"][0]["pattern"] = server_id
-                rule["pattern"] = f"MEMORYnode{server_num}"
-                rule["tooltiplabel"] = "Memory Usage%"
+                rule["pattern"] = f"MEMORYGPU{server_num}"
+                rule["tooltipLabel"] = "Memory Usage%"
                 dashboard["panels"][0]["rulesData"]["rulesData"].append(rule)
 
             elif rulenum == 2:
@@ -40,29 +41,29 @@ with open('servers.txt') as f:
                 rule["alias"] = "CPU Usage"
                 rule["linkData"][0]["pattern"] = server_id
                 rule["shapeData"][0]["pattern"] = server_id
-                rule["pattern"] = f"CPUnode{server_num}"
-                rule["tooltiplabel"] = "CPU Usage"
+                rule["pattern"] = f"CPUGPU{server_num}"
+                rule["tooltipLabel"] = "CPU Usage"
                 dashboard["panels"][0]["rulesData"]["rulesData"].append(rule)
 
-dashboard["panels"][0]["flowchartsData"]["flowcharts"][0]["xml"] = diagram
+dashboard["panels"][0]["flowchartsData"]["flowcharts"][0]["xml"] = diagram.strip()
 
 with open('data.json', 'w', encoding='utf-8') as f:
     json.dump(dashboard, f,ensure_ascii=False, indent=4)
 
 
 
-with open('data.json') as json_file:
-    json_data = json.load(json_file)
-print(type(json_data))
-headers = {"Accept": "application/json",
-           "Content-Type": "application/json",
-           "Authorization": "Bearer eyJrIjoiektabHdnNkk2NjhpbnZzVmlUQ0U5NHhpdDZvNEtpWEkiLCJuIjoidGVzdDIiLCJpZCI6MX0="
-           }
+#with open('data.json') as json_file:
+#    json_data = json.load(json_file)
+#print(type(json_data))
+#headers = {"Accept": "application/json",
+#           "Content-Type": "application/json",
+#           "Authorization": "Bearer eyJrIjoiektabHdnNkk2NjhpbnZzVmlUQ0U5NHhpdDZvNEtpWEkiLCJuIjoidGVzdDIiLCJpZCI6MX0="
+#           }
 
 #r = requests.get("http://www.localhost:3005", headers=headers)
 #print(r.status_code)
 
-url = "http://www.localhost:3005/api/dashboards/db"
-r = requests.post(url, data=json.dumps(json_data), headers=headers)
+#url = "http://www.localhost:3005/api/dashboards/db"
+#r = requests.post(url, data=json.dumps(json_data), headers=headers)
 #p = requests.post(url, headers=headers, data=json.dumps(open('data.json', 'rb')))
-print(r.status_code)
+#print(r.status_code)
