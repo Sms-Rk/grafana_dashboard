ce22098a228d29194f147c3d59962613855de63d
Sms-Rk <smsrk741@gmail.com>
2022-12-14 16:00:30 +0330

    FIX SERVER NAMES

--- grafana.py
+++ grafana.py
@@ -5,13 +5,14 @@ from diagram import diagram
 from string import Template
 
 
+
 dashboard = json.loads(dashboard_temp)
 #rule = json.loads(rule_temp)
 link_url = Template("https://grafana.tip.co.ir/d/rYdddlPWk/per-gpu-metrics?orgId=1&refresh=1m&var-server=$server&from=now-15m&to=now")
 with open('servers.txt') as f:
     for index, line in enumerate(f):
         server_id=(line.strip().split(',')[0])
-        server_num=(line.strip().split(',')[1][-2:])
+        server_num=(line.strip().split(',')[1])
         rulenum = 0
         for rulenum in range(3):
             if rulenum == 0:
@@ -21,7 +22,7 @@ with open('servers.txt') as f:
                 rule["shapeData"][0]["pattern"] = server_id
                 rule["pattern"] = f"GPUnode{server_num}"
                 rule["tooltiplabel"] = "GPU Utils"
-                link = f"node{server_num}"
+                link = f"GPU{server_num}"
                 rule["linkData"][0]["linkUrl"] = link_url.substitute(server=link)
                 dashboard["panels"][0]["rulesData"]["rulesData"].append(rule)
 
@@ -48,5 +49,20 @@ dashboard["panels"][0]["flowchartsData"]["flowcharts"][0]["xml"] = diagram
 with open('data.json', 'w', encoding='utf-8') as f:
     json.dump(dashboard, f,ensure_ascii=False, indent=4)
 
-#r = requests.post('http://localhost:3005/api/dashboards/db', json=json.dumps(dashboard_temp))
+
+
+with open('data.json') as json_file:
+    json_data = json.load(json_file)
+print(type(json_data))
+headers = {"Accept": "application/json",
+           "Content-Type": "application/json",
+           "Authorization": "Bearer eyJrIjoiektabHdnNkk2NjhpbnZzVmlUQ0U5NHhpdDZvNEtpWEkiLCJuIjoidGVzdDIiLCJpZCI6MX0="
+           }
+
+#r = requests.get("http://www.localhost:3005", headers=headers)
 #print(r.status_code)
+
+url = "http://www.localhost:3005/api/dashboards/db"
+r = requests.post(url, data=json.dumps(json_data), headers=headers)
+#p = requests.post(url, headers=headers, data=json.dumps(open('data.json', 'rb')))
+print(r.status_code)
